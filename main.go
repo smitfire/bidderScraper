4de5e8b860a368cb42f31d7fe3b6d1efb322ed23
Nicolaas Smit <nick.smit@rtk.io>
2020-05-27 12:08:07 +0200

    initial mod dependencies

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+	"github.com/microcosm-cc/bluemonday"
+	"gopkg.in/russross/blackfriday.v2"
 	"github.com/voodoolabs/bidderScraper/cmd"
 )
+
+unsafe := blackfriday.Run(input)
+html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 
 func main() {
 
